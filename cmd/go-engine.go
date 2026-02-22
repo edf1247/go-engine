9be56c4e00b8cb8f91ae5233397ed9d6310a2482
Ethan Farber <edf1247@gmail.com>
2026-02-22 13:21:35 -0500

    updated structure

--- cmd/go-engine.go
+++ cmd/go-engine.go
@@ -1,6 +1,10 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+    camera "github.com/edf1247/go-engine/internal/camera"
+    colors "github.com/edf1247/go-engine/internal/colors"
+    rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 const WIDTH = 800
 const HEIGHT = 450
@@ -9,7 +13,7 @@ func main() {
   rl.InitWindow(WIDTH, HEIGHT, "Go Engine")
   defer rl.CloseWindow()
 
-  rl.SetTargetFPS(60)
+  rl.SetTargetFPS(144.0)
   var camInit = false
   var mainCamera rl.Camera3D
 
@@ -18,16 +22,16 @@ func main() {
     rl.ClearBackground(BLACK)
     
     if !camInit {
-        mainCamera = InitCamera()
+        mainCamera = camera.InitCamera()
         camInit = true
     }
     rl.UpdateCamera(&mainCamera, rl.CameraFree)
     rl.BeginMode3D(mainCamera)
     
-    moveCamera(&mainCamera)
+    camera.moveCamera(&mainCamera)
 
-    rl.DrawCube(rl.Vector3{0, 0, 0}, 2.0, 2.0, 2.0, WHITE)
-    rl.DrawCubeWires(rl.Vector3{0, 0, 0}, 2.0, 2.0, 2.0, MAROON)
+    rl.DrawCube(rl.Vector3{0, 0, 0}, 2.0, 2.0, 2.0, colors.WHITE)
+    rl.DrawCubeWires(rl.Vector3{0, 0, 0}, 2.0, 2.0, 2.0, colors.MAROON)
     rl.DrawGrid(10.0, 1.0)
 
     rl.EndMode3D()
